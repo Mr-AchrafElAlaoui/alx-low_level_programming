16191e218898246d255e10992af8c0d2fc5a54df
Mr.ElAlaoui <hraf23@gmail.com>
2024-04-15 17:15:22 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+    hash_table_t *ht;
+    unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+    /* Allocate memory for the hash table */
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+    ht->size = size;
 
-	return (ht);
+    /* Allocate memory for the array of hash nodes */
+    ht->array = malloc(sizeof(hash_node_t *) * size);
+    if (ht->array == NULL) {
+        free(ht); /* Free the previously allocated memory */
+        return (NULL);
+    }
+
+    /* Initialize each element of the array to NULL */
+    for (i = 0; i < size; i++)
+        ht->array[i] = NULL;
+
+    return (ht);
 }
