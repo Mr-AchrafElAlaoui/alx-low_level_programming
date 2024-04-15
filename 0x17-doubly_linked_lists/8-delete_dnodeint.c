1ec28c67e8e998e563698e79c384508ffe48998e
Mr.ElAlaoui <hraf23@gmail.com>
2024-04-15 16:14:53 +0100

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,47 +1,45 @@
 /*
- * File: 8-delete_dnodeint.c
- * Auth: Nicolas Peter Wanjau
+ * delete_dnodeint_at_index.c - Deletes a node from a dlistint_t
+ *                               at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
+ *
+ * Returns: Upon success - 1.
+ *          Otherwise - -1.
  */
 
 #include "lists.h"
 
-/**
- * delete_dnodeint_at_index - Deletes a node from a dlistint_t
- *                            at a given index.
- * @head: A pointer to the head of the dlistint_t.
- * @index: The index of the node to delete.
- *
- * Return: Upon success - 1.
- *         Otherwise - -1.
- */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
 
-	if (*head == NULL)
-		return (-1);
+	if (!*head)
+		return -1;
 
-	for (; index != 0; index--)
+	while (index && tmp)
 	{
-		if (tmp == NULL)
-			return (-1);
 		tmp = tmp->next;
+		index--;
 	}
+
+	if (!tmp)
+		return -1;
 
 	if (tmp == *head)
 	{
 		*head = tmp->next;
-		if (*head != NULL)
+		if (*head)
 			(*head)->prev = NULL;
 	}
 
 	else
 	{
 		tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
+		if (tmp->next)
 			tmp->next->prev = tmp->prev;
 	}
 
-	free(tmp);
-	return (1);
+	kfree(tmp);
+	return 1;
 }
