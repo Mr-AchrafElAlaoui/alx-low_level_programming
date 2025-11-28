42fd84481cafee09200971d09b5e72024cea56c3
Mr-achraf <hraf23@gmail.com>
2025-11-28 12:42:17 +0100

    Program prints letters except 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower_char;
+
+	lower_char = 'a';
+
+	while (lower_char <= 'z')
+	{
+		if (lower_char != 'q' && lower_char != 'e')
+			putchar(lower_char);
+
+		lower_char++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
