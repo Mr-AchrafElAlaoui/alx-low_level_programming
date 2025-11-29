7e363a4b2d0fb374edc2ef9e42f1175789388a39
Mr-achraf <hraf23@gmail.com>
2025-11-29 13:12:33 +0100

    Program prints lower alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lower_char;
+
+	lower_char = 122;
+
+	while (lower_char >= 97)
+	{
+		putchar(lower_char);
+		lower_char--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
