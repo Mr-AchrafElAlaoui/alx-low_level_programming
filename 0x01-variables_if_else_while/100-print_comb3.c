16c7fd4856d17d1babf69725b70f7092dc59e1bc
Mr-achraf <hraf23@gmail.com>
2025-11-29 14:17:05 +0100

    Program prints all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first_digit;
+	int second_digit;
+
+	for (first_digit = 48; first_digit < 57; first_digit++)
+	{
+		for (second_digit = first_digit + 1; second_digit <= 57; second_digit++)
+		{
+			putchar(first_digit);
+			putchar(second_digit);
+
+			if (first_digit != 56 || second_digit != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
