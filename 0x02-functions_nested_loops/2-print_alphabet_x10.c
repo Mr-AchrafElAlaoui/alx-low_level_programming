6ce1e3cd979486c389bab18a62d05622243816b3
Mr-achraf <hraf23@gmail.com>
2025-12-01 17:38:31 +0100

    Function prints alphabet in lowercase x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+
+	count = 0;
+
+	while (count < 10)
+	{
+		print_alphabet();
+	}
+}
