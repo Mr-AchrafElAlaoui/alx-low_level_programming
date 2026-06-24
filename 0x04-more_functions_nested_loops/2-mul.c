8ff9c6b5ecf4a56397f3f37c9e5f453c509193f2
Achraf <hraf23@gmail.com>
2026-06-24 13:25:41 +0100

    Add function definition

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: First operand
+ * @b: second operand
+ *
+ * Return: The multiplication of the two parameters
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
