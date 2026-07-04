4d9d3f4af7d0bac6a168c1875bf1b92344043f64
Achraf <hraf23@gmail.com>
2026-07-04 14:23:54 +0100

    Add function definition

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line.
+ * @str: Pointer of string
+ */
+void print_rev(char *str)
+{
+	int len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len >= 0)
+	{
+		_putchar(str[len]);
+		len--;
+	}
+
+	_putchar('\n');
+}
