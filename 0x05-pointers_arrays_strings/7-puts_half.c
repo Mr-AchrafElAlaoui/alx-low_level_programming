a63d401acad72f519959f61e7032002b1feb32d5
Achraf <hraf23@gmail.com>
2026-07-06 17:19:46 +0100

    Modifie start index

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,7 @@
  */
 void puts_half(char *str)
 {
-	int len, i;
+	unsigned int len, i;
 
 	len = 0;
 
@@ -24,7 +24,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = (len - 1) / 2; i < len; i++)
+		for (i = (len + 1) / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
