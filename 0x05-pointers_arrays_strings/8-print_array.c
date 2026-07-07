1fc84df5ff361a4e910a1f9d4c57e958979feee6
Achraf <hraf23@gmail.com>
2026-07-07 14:52:52 +0100

    Separate new line print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,15 +8,17 @@
  */
 void print_array(int *a, int n)
 {
+	int i;
+
 	if (n > 0)
 	{
-		int i;
-
 		for (i = 0; i < (n - 1); i++)
 		{
 			printf("%d, ", a[i]);
 		}
 
-		printf("%d\n", a[i]);
+		printf("%d", a[i]);
 	}
+
+	printf("\n");
 }
