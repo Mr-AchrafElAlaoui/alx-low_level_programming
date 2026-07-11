46aae0fe57337fecc6a8fa3c04cb12abd30668f6
Achraf <hraf23@gmail.com>
2026-07-11 16:00:36 +0100

    Add function definition

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - Appends the src string to the dest string
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: The pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+
+	ptr = dest;
+	
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	while (*src != '\0')
+	{
+		*ptr = *src;
+
+		src++;
+		ptr++;
+	}
+
+	*ptr = '\0';
+
+	return (dest);
+}
