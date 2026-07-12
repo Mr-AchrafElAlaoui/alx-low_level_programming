3aa9b332ca704b44768b37879e75b854b41f0611
Achraf <hraf23@gmail.com>
2026-07-12 15:08:24 +0100

    Add function definition

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string from src to dest
+ * @dest: The destination buffer
+ * @src: The source of string to be copied
+ * @n: The number of bytes to copie
+ *
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr;
+
+	ptr = dest;
+
+	while (*ptr != '\0' && n > 0 && *src != '\0')
+	{
+		*ptr = *src;
+
+		ptr++;
+		src++;
+		n--;
+	}
+	
+	while (n > 0 && *ptr != '\0')
+	{
+		*ptr = '\0';
+		
+		ptr++;
+		n--;
+	}
+
+	return (dest);
+}
