ff273e21d0026af4c30612ee0c01860072d863b3
Achraf <hraf23@gmail.com>
2026-07-15 14:45:37 +0100

    Add function definition

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @str: The string to be encoded
+ *
+ * Retrun: Pointer to char
+ */
+char *leet(char *str)
+{
+	char *ptr;
+	char *alp;
+	char *code;
+	int i;
+
+	alp = "aAeElLoOtT";
+	code = "4433110077";
+
+	ptr = str;
+
+	while (*ptr != '\0')
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (*ptr == alp[i])
+			{
+				*ptr = code[i];
+				
+				break;
+			}
+		}
+
+		ptr++;
+	}
+
+	return (str);
+}
