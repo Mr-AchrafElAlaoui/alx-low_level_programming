ec30bbb83588fb17404b6bae6209b66c1550b9f9
Achraf <hraf23@gmail.com>
2026-07-21 18:09:49 +0100

    Follow DRY principle

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,101 +13,60 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i;
 	int j;
 	int k;
+	int num1;
+	int num2;
 	int sum;
 	int carry;
-	char *ptr;
-	char *ptr2;
-	char *pn1;
-	char *pn2;
 	char tmp;
 
-	pn1 = n1;
-	pn2 = n2;	
-	ptr = r;
 	carry = 0;
 	i = 0;
 	j = 0;
 	k = 0;
 
-	while (*pn1 != '\0')
+	while (n1[i] != '\0')
 	{
 		i++;
-		pn1++;
 	}
 
-	while (*pn2 != '\0')
+	while (n2[j] != '\0')
 	{
 		j++;
-		pn2++;
-	}
-
-	if (i > size_r || j > size_r)
-	{
-		return (0);
 	}
 
 	i--;
 	j--;
 	
-	while (i >= 0 && (size_r) > k)
+	while (i >= 0 || j >= 0 || carry > 0)
 	{
-		if (j >= 0)
+		if ((size_r - 1) <= k)
 		{
-			sum = (n1[i] - '0') + (n2[j]  - '0') + carry;
-		}
-		else
-		{
-			sum = (n1[i] - '0') + carry;
+			return (0);
 		}
 
+		num1 = (i >= 0) ? (n1[i] - '0') : 0;
+		num2 = (j >= 0) ? (n2[j] - '0') : 0;
+		
+		sum = num1 + num2 + carry;
 		carry = sum / 10;
-		*ptr = ((sum % 10) + '0');
+
+		r[k] = ((sum % 10) + '0');
 
 		i--;
 		j--;
 		k++;
-		ptr++;
 	}
 
-	while (j >= 0 && (size_r) > k)
+	r[k] = '\0';
+	k--;
+
+	for (i = 0; i < k; k--)
 	{
-		sum = (n2[j] - '0') + carry;
+		tmp = r[i];
+		r[i] = r[k];
+		r[k] = tmp;
 
-		carry = sum / 10;
-
-		*ptr = ((sum % 10) + '0');
-
-		j--;
-		k++;
-		ptr++;
-	}
-
-	if(carry != 0  && (size_r) > k )
-	{
-		*ptr = (carry + '0');
-		ptr++;
-		k++;
-	}
-
-	*ptr = '\0';
-
-	if ((size_r - 1) < k)
-	{
-		return (0);
-	}
-
-	ptr--;
-
-	ptr2 = r;
-
-	while (ptr2 < ptr)
-	{
-		tmp = *ptr2;
-		*ptr2 = *ptr;
-		*ptr = tmp;
-
-		ptr2++;
-		ptr--;
+		i++;
 	}
 
 	return (r);
