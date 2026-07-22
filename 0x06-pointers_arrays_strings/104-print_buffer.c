cb8f2cc91653b7b4d74e49bee62fb3790a07d660
Achraf <hraf23@gmail.com>
2026-07-22 17:34:42 +0100

    Add function definition

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,1 +1,52 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer
+ * @b: Pointer to buffer
+ * @size: The size of buffer to print
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+	int j;
+	char *chunk;
+
+	for (i = 0; i < size; i += 10)
+	{
+		chunk = b + i;
+		printf("%08x:", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (j % 2 == 0)
+			{
+				printf(" ");
+			}
+
+			if (i + j < size)
+			{
+				printf("%02x", chunk[j]);
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+
+		printf(" ");
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (chunk[j] >= ' ' && chunk[j] <= '~')
+					printf("%c", chunk[j]);
+				else
+					printf(".");
+			}
+		}
+
+		printf("\n");
+	}
+}
