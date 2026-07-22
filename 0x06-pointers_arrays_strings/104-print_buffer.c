1f902481504944d3244328063d666df0be6d7649
Achraf <hraf23@gmail.com>
2026-07-22 17:38:05 +0100

    Limite function in 40 lines

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,21 +8,19 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i;
-	int j;
+	int i, j;
 	char *chunk;
 
 	for (i = 0; i < size; i += 10)
 	{
 		chunk = b + i;
+
 		printf("%08x:", i);
 
 		for (j = 0; j < 10; j++)
 		{
 			if (j % 2 == 0)
-			{
 				printf(" ");
-			}
 
 			if (i + j < size)
 			{
