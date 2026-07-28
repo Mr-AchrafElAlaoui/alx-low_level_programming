ab0aae4e86a43467df51506a9f4e8505a16e674b
Achraf <hraf23@gmail.com>
2026-07-28 17:06:10 +0100

    Add function Doc

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * _memeset - fills memory with a constant byte
+ * @s: Memory area
+ * @b: Constant byte
+ * @n: Bytes of memory to fill
+ *
+ * Return: Pointer to the memory area s
+ */
+char *_memeset(char *s, char b, unsigned int n)
+{
+
+}
