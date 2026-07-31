ff4706e01ee8d15c19a65a44ea98cb6320b709ad
Achraf <hraf23@gmail.com>
2026-07-31 14:33:52 +0100

    Add function definition

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,11 +3,22 @@
 /**
  * _strchr - Locates a character in a string
  * @s: String
- * @c: character
+ * @c: Character
  *
- * Return: Pointer to the first occurrence of thr character c
+ * Return: Pointer to the first occurrence of thr character c,
+ * NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
 
+		s++;
+	}
+
+	return (0);
 }
