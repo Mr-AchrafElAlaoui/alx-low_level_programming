73c82ad41ce3fa15b204068a60bece07b16907cb
Achraf <hraf23@gmail.com>
2026-08-08 16:22:56 +0100

    Add function definition

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y
+ * @x: The base number
+ * @y: The exponent number
+ *
+ * Retrun: The result of x raised to the power of y,
+ * otherwise -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
