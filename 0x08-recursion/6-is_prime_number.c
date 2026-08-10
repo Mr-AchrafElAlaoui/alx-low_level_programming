2e89b4816bfb91b703e32594467261fe5157f613
Achraf <hraf23@gmail.com>
2026-08-10 17:41:11 +0100

    Add function documentation

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
 /**
+ * prime_number_accumulator - Tail-recursive helper to check for prime
+ * @n: The number we are checking
+ * @a: the current odd number we are testing as a divisor
  *
+ * Description: Uses Tail Call Optimization. @a It steps by 2 (skipping evens)
+ * and stops at the square root (a * a > n) to save CPU cycles.
+ *
+ * Return: 1 if prime, 0 if not.
  */ 
 int prime_number_accumulator(int n, int a)
 {
@@ -18,7 +25,7 @@ int prime_number_accumulator(int n, int a)
  * is_prime_number - Check if the number is a prime number
  * @n: The number to check
  *
- * Return: 1 if the number is prime number, Otherwise return 0
+ * Return: 1 if the number is prime number, otherwise return 0
  */
 int is_prime_number(int n)
 {
