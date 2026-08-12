556dd9808530856d5bb93fc27c298efd571ada2d
Achraf <hraf23@gmail.com>
2026-08-12 16:59:05 +0100

    Add 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares the identicality of two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
