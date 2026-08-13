980a6d40aaccee67337d7447dbd45073aeed4162
Achraf <hraf23@gmail.com>
2026-08-13 15:29:47 +0100

    Respect betty style

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,10 +20,10 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 
-		if (wildcmp(s1, s2 +1) == 1)
+		if (wildcmp(s1, s2 + 1) == 1)
 			return (1);
 
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2) == 1 )
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2) == 1)
 			return (1);
 	}
 
