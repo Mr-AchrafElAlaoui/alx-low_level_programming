bfdf00d1d2885a52493a65f3086ccc5d1dea6744
Achraf <hraf23@gmail.com>
2026-08-19 19:24:05 +0100

    Update argc output

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, __attribute__((unused)) char **argv)
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", (argc));
+	printf("%d\n", argc - 1);
 	return (0);
 }
