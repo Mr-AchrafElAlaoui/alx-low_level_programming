399d8cf3999c86ece2a03f0794961a3025c481e7
Achraf <hraf23@gmail.com>
2026-08-21 16:23:47 +0100

    Program adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point, adds positive numbers
+ * @argc: The number of command line arguments passed
+ * @argv: Array containing command line arguments passed
+ *
+ * Return: 0 (Success), 1 (Error) contains symbols that are not digits.
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+	int i;
+	
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!(atoi(argv[i])))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
