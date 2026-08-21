5ff149898b24f803d452affdacc16f48ea51cd69
Achraf <hraf23@gmail.com>
2026-08-21 16:46:36 +0100

    Respect betty style

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point, prints the minimum number of coins to make change 
+ * main - Entry point, prints the minimum number of coins to make change
  * for an amount of money.
  * @argc: The number of command line arguments passed
  * @argv: Array containing the program command line arguments passed
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
 	int coins;
 	int cents;
 
-	if (argc != 2 )
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	coins = 0;
 	cents = atoi(argv[1]);
 
@@ -28,25 +28,21 @@ int main(int argc, char *argv[])
 		cents -= 25;
 		coins++;
 	}
-
 	while (cents >= 10)
 	{
 		cents -= 10;
 		coins++;
 	}
-
 	while (cents >= 5)
 	{
 		cents -= 5;
 		coins++;
 	}
-
 	while (cents >= 2)
 	{
 		cents -= 2;
 		coins++;
 	}
-
 	while (cents >= 1)
 	{
 		cents -= 1;
@@ -54,6 +50,5 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", coins);
-
 	return (0);
 }
