a6a22c1841c067cd46ebfb60c5c23eaf2fc2e463
Achraf <hraf23@gmail.com>
2026-08-27 16:53:32 +0100

    Add function definition

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocates a 2 dimensional array of integers.
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Pointer to 2 dimensional array of integers,
+ * NULL if width or height is 0 or negative.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0  || height <= 0)
+		return (NULL);
+	
+	grid = malloc(sizeof(int*) * width);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width);
+
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
