5bfb6abef9a129ccb4645713bab1d956ded822a0
Achraf <hraf23@gmail.com>
2026-09-01 17:43:01 +0100

    Add function documentation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 
 /**
+ * strtow - Splits string into words.
+ * @str: String to split.
  *
- */ 
+ * Return: Pointer to array of strings (words), NULL if fails
+ */
+char **strtow(char *str)
+{
+
+}
