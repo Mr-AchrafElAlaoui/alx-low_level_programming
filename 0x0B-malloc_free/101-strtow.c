a4b0e0af5f9efbdeb7e7bc81afdbfa844e2700e2
Achraf <hraf23@gmail.com>
2026-09-02 18:33:40 +0100

    Add function definition

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,5 +9,80 @@
  */
 char **strtow(char *str)
 {
+	char **words;
+	int i, k;
+       	int size, len;
+	int word;
 
+	if (str == NULL)
+		return (NULL);
+	
+	i = 0;
+	len = 0;
+	size = 0;
+	word = 0;
+
+	while (str[i] != '\0')
+	{
+		while (str[i] != ' ' && str[i] != '\0')
+		{
+			word = 1;
+			i++;
+		}
+
+		if (word == 1)
+		{
+			size++;
+			word = 0;
+		}
+		
+		if (str[i] != '\0')
+			i++;
+	}
+	
+	words = malloc(sizeof(char *) * (size + 1));
+
+	if (words == NULL)
+		return (NULL);
+	k = 0;
+	word = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		while ( str[k] != ' ' && str[k] != '\0')
+		{
+			word = 1;
+			len++;
+			k++;
+		}
+
+		if (word == 1)
+		{
+			words[i] = malloc(len + 1);
+			word = 0;
+			len = 0
+		}
+		if (words[i] == NULL)
+			return (NULL);
+		k++;
+	}
+	
+	for (i = 0; i < size; i++)
+	{
+		k = 0;
+
+		while (*str != ' ' && *str != '\0')
+		{
+			words[i][k] = *str;
+			k++;
+			str++;
+		}
+
+		words[i][k] = '\0';
+		str++;
+	}
+
+	words[i] = NULL;
+
+	return (words);
 }
